8e6f6a48f65d0a21f8e9fd0d15e33a7dcea20f50
zdaoxb <89411223+zdaoxb@users.noreply.github.com>
2024-01-08 19:46:16 +0800

    Update index.tsx

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -2,7 +2,7 @@ import { memo, useEffect } from 'react';
 
 const PageTitle = memo<{ title: string }>(({ title }) => {
   useEffect(() => {
-    document.title = title ? `${title} · LobeChat` : 'LobeChat';
+    document.title = title ? `${title} · 九典 AI` : '九典 AI';
   }, [title]);
 
   return null;
