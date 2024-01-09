ee718226d42be705b30b550e33bf9793d81ac0c8
zdaoxb <89411223+zdaoxb@users.noreply.github.com>
2024-01-09 16:38:39 +0800

    Update metadata.ts

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 import pkg from '../../package.json';
 
-const title = 'LobeChat';
+const title = '九典 AI';
 const { description, homepage } = pkg;
 const metadata: Metadata = {
   appleWebApp: {
